85be3fa34012cf4e6f150c6835bf3178a66f9cf3
David <davidotvos365@gmail.com>
2024-01-07 22:09:55 +0100

    Adding Bycicle class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 class Auto:
     def __init__(self, id, type, doors, brand):
         self.id = id
@@ -11,8 +12,20 @@ class Auto:
         return f"ID: {self.id}, Type: {self.type}, Doors: {self.doors}, Brand: {self.brand}"
 
 
+class Bycicle:
+    def __init__(self, id, type, load_capacity, brand):
+        self.id = id
+        self.type = type
+        self.load_capacity = load_capacity
+        self.brand = brand
+
+    def __str__(self):
+        return f"ID: {self.id}, Type: {self.type}, Load capacity: {self.load_capacity}, Brand: {self.brand}"
+
+
 def main():
     print("Py3")
+
 
 ##############################################################################
 
